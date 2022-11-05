9fa181f3260eb1171f2b1d5835fe21e6cda164bd
Mauricio <mauricioaguilera5555@gmail.com>
2022-11-05 19:43:04 -0300

    Ultimos cambios antes de merge a main

--- blog/models.py
+++ blog/models.py
@@ -7,4 +7,11 @@ class Configuracion(models.Model):
     titulo_principal=models.CharField(max_length=30, default='', blank=False)
     subtitulo_principal=models.CharField(max_length=30, default='')
 
+class Post (models.Model):
+    title=models.CharField(max_lenght=100)
+    short_content = models.Charfield(max_lenght=255)
+    content=models.TextField(max_length=3000)
+    date_published=models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"id:{self.id}, title:{self.title}"
